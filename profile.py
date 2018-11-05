f5ac883373d63101b798e02bacaa13482e7d1134
felixguerrero12 <felixguerrero12@gmail.com>
2018-11-05 10:03:13 -0500

    First Commit using Pycharm: Create functions for profile.py. It still needs more additions.

--- profile.py
+++ profile.py
@@ -1,9 +1,19 @@
 import json
 import passgen
-import pprint
+import sys
 
-passwords = {}
-for i in range(5):
-	passwords[i] = passgen.gen(15)
-results = {"passwords": passwords}
-print (results)
+
+def gen(passwords):
+    for i in range(5):
+	    passwords[i] = passgen.gen(15)
+    results = {"passwords": passwords}
+    return results
+
+
+def main(argv):
+    passwords = {}
+    print (gen(passwords))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
