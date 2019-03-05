f86482d5ed34100c0c62b7d7f4664b5555af180a
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2019-03-05 13:06:48 -0500

    Update gen_netsh.py

--- gen_netsh.py
+++ gen_netsh.py
@@ -2,15 +2,15 @@ import sys, ipaddress
 
 
 def print_rule(x):
-    print "netsh advfirewall firewall add rule name=" + str(x) + " dir=in interface=any action=block remoteip=" + str(x)
+    print("netsh advfirewall firewall add rule name=" + str(x) + " dir=in interface=any action=block remoteip=" + str(x))
 
 
 def main(argv):
     w_str = list(sys.argv[1:])
     x = list(ipaddress.IPv4Network('192.168.1.0/24'))[1:-1]
     for ip in x:
-	if str(ip) not in w_str:
-	    print_rule(ip)
+         if str(ip) not in w_str:
+                print_rule(ip)
 
 
 if __name__ == "__main__":
