5a0de8a0c777369fea6cbc9b63e64530e7cf9b10
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2019-03-06 11:01:50 -0500

    Update gen_iptables.py

--- gen_iptables.py
+++ gen_iptables.py
@@ -1,23 +1,32 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys, ipaddress
+import ipaddress
+import sys
+import os
+
 
 def print_rule_input(x):
     print("iptables -A INPUT -p tcp -s " + str(x) + " -j ACCEPT")
 
+
 def print_rule_output(x):
     print("iptables -A OUTPUT -p tcp -s " + str(x) + " -j ACCEPT")
+
 
 def print_closure():
     return "iptables -A INPUT -j DROP" + "\n" + "iptables -A OUTPUT -j DROP"
 
+
 def main(argv):
-    w_str = list(sys.argv[1:])
-    x = list(ipaddress.IPv4Network('192.168.1.0/24'))[1:-1]
+    blacklist = "bipaddr.txt"
+    if os.path.exists(blacklist) and os.path.getsize(blacklist) > 0:
+        with open(blacklist, 'r') as f:
+            w_str = f.read().splitlines()
+    else:
+        w_str = list(sys.argv[1:])
+    x = list(ipaddress.IPv4Network(u'192.168.1.0/24'))[1:-1]
     for ip in x:
         if str(ip) in w_str:
-            input_table=print_rule_input(ip)
-            output_table=print_rule_output(ip)
+            input_table = print_rule_input(ip)
+            output_table = print_rule_output(ip)
     print(print_closure())
 
 if __name__ == "__main__":
