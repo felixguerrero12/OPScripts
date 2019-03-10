a221d2e955603a364f08b172fa2cff81c9270fed
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2019-03-10 16:19:11 -0400

    Update sniff_dns.py

--- sniff_dns.py
+++ sniff_dns.py
@@ -1,21 +1,19 @@
 #! /usr/bin/python
-
 from scapy.all import *
 import sys
 
-try:
-        interface = 'en0'
-except KeyboardInterrupt:
-        print("[*] User Requested Shutdown...")
-        print("[*] Exiting...")
-        sys.exit(1)
 
 def capture(pkt):
         if IP in pkt:
                 ip_src = pkt[IP].src
                 ip_dst = pkt[IP].dst
                 if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
-                        print(ip_src + " - > " + ip_dst + " " + "Domain requested: " + str(pkt.getlayer(DNS).qd.qname))
+                        print(ip_src + " - > " + ip_dst + " " + "Domain: " + str(pkt.getlayer(DNS).qd.qname))
 
-sniff(iface = interface, prn = capture, store = 0)
-print("\n[*] Shutting Down...")
+def main():
+	interface = 'en0'
+	sniff(iface = interface, prn = capture, store = 0)
+	print("\n[*] Shutting Down...")
+
+if __name__ == "__main__":
+    main()
