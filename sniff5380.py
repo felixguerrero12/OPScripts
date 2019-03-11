22fe0f8963115d9d487f44501b21fce457432053
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2019-03-10 21:23:50 -0400

    PEP8 Update.

--- sniff5380.py
+++ sniff5380.py
@@ -2,11 +2,13 @@
 from scapy.all import *
 import sys
 
+
 def extract_headers_from_request_payload(payload):
     try:
         return payload.split('\r\n\r\n')[0].split('\r\n')
     except ValueError:
         return payload.split('\r\n')
+
 
 def find_url_from_headers(headers):
     host = ''
@@ -31,12 +33,11 @@ def capture(pkt):
                     payload = pkt[Raw].load.decode('utf-8', 'ignore')
                     print(ip_src + " - > " + ip_dst + " " + "Full URL: " + find_url_from_headers(extract_headers_from_request_payload(payload)))
 
-#                    ret = "\n".join(pkt.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
-#                    print(ret)
+
 def main():
-	interface = 'en0'
-	sniff(iface = interface, prn = capture, store = 0)
-	print("\n[*] Shutting Down...")
+    interface = 'en0'
+    sniff(iface=interface, prn=capture, store=0)
+    print("\n[*] Shutting Down...")
 
 if __name__ == "__main__":
     main()
